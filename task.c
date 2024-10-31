731b041e1c2d37f18f2ae7c4f581c537e072c418
lihua-0ps <2100244776@qq.com>
2024-10-31 21:22:47 +0800

    first commit

--- task.c
+++ task.c
@@ -22,7 +22,25 @@ int main(void)
     在下方编写你的代码
     """
     
-
+    while(1)
+    {
+        P0=0xFE;//1111 1110
+        Delay1ms(500);
+        P0=0xFD;//1111 1101
+        Delay1ms(500);
+        P0=0xFB;//1111 1011
+        Delay1ms(500);
+        P0=0xF7;//1111 0111
+        Delay1ms(500);
+        P0=0xEF;//1110 1111
+        Delay1ms(500);
+        P0=0xDF;//1101 1111
+        Delay1ms(500);
+        P0=0xBF;//1011 1111
+        Delay1ms(500);
+        P0=0x7F;//0111 1111
+        Delay1ms(500);
+    }
  
 
     return 0;
